2abbadf4b0ad984121d2a12458c176d222dde2c4
Damla Kara <kdamla41@gmail.com>
2023-11-02 15:58:22 +0300

    set cookie options for production

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,7 +19,7 @@ const createSendToken = (user,statusCode,req, res) => {
 
   const token = signToken(user._id)
   const cookieOptions =  {
-    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
+    maxAge: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
     httpOnly: true,
     secure: req.secure
   }
