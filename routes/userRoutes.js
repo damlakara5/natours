6bbaf94510a5e38ac4c39f73a2bf599b38dd69fe
Damla Kara <kdamla41@gmail.com>
2023-11-03 15:21:36 +0300

    get all the reviews for user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const userController = require("../controllers/userController")
 const authController = require("../controllers/authController")
+const reviewRouter = require("./../routes/reviewRoutes")
 
 
 const router = express.Router()
